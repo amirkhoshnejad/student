970cbda55cedde29d8577c6218887d4ab76fbd7c
amirkhoshnejad <amir.khoshnezhad@gmail.com>
2020-05-25 01:08:28 +0430

    start project

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] student =System.IO.File.ReadAllLines(@"E:\projects\C#_Project\csharp\student\studentProfile.txt");
+            int num=0;
+            int n=10;
+            var array = new List<studentNames>();
+            for (int i = 0; i < n; i++)
+            {
+                var w =new studentNames();
+                w.name="amir";
+                w.age="24";
+                w.weight="80";
+                w.height="190";
+                array.Add(new studentNames());
+            }
+        }
+    }
+}
---
+++ studentNames.cs
@@ -0,0 +1,10 @@
+
+
+namespace student{
+    class studentNames{ 
+        public string name { get; set; }
+        public string age { get; set; }
+        public string weight { get; set; }
+        public string height { get; set; }    
+  }
+}
