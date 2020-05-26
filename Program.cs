e970869bb516fa2a3fc99dea58ebe5ab9b4051fd
amirkhoshnejad <amir.khoshnezhad@gmail.com>
2020-05-26 16:23:58 +0430

    create array with objects

--- Program.cs
+++ Program.cs
@@ -1,24 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace student
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace student{
+    class Program {
+        static void Main(string[] args){
             string[] student =System.IO.File.ReadAllLines(@"E:\projects\C#_Project\csharp\student\studentProfile.txt");
-            int num=0;
-            int n=10;
+            int num = Int32.Parse(student[0]),n =num*4;
             var array = new List<studentNames>();
-            for (int i = 0; i < n; i++)
-            {
-                var w =new studentNames();
-                w.name="amir";
-                w.age="24";
-                w.weight="80";
-                w.height="190";
+            for (int i = 0,j=1; i <= num &&  j<=n; i++){
                 array.Add(new studentNames());
+                array[i].name=student[j++];
+                array[i].age=student[j++];
+                array[i].weight=student[j++];
+                array[i].height=student[j++];
             }
         }
     }
